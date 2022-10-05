ffd4077af476e33a836b408143209ccbda553bd1
natashapettinger <natasha.pettinger@gmail.com>
2022-10-05 15:26:27 -0600

    started adding redux in

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import './assets/main.css';
 import App from './App';
-import './assets/main.css'
+import store from './app/store';
+import { Provider } from 'react-redux';
 
 
 const root = createRoot(document.getElementById('root'));
 
-root.render(<App />);
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
