2160b96c1355ce4267c2eef4e2e30375e99953e1
natashapettinger <natasha.pettinger@gmail.com>
2022-10-05 15:27:46 -0600

    forgot one file

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,18 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit'
 import homeReducer from '../features/home/homeSlice';
 import inventoryReducer from '../features/inventory/inventorySlice';
-//import processingReducer from '../features/processing/processingSlice';
-//import productionReducer from '../features/production/productionSlice';
-//import storageReducer from '../features/storage/storageSlice';
-//import ttbReducer from '../features/ttb/ttbSlice';
+import processingReducer from '../features/processing/processingSlice';
+import productionReducer from '../features/production/productionSlice';
+import storageReducer from '../features/storage/storageSlice';
+import ttbReducer from '../features/ttb/ttbSlice';
 
 export default configureStore({
   reducer: {
     home: homeReducer,
     inventory: inventoryReducer,
-    //processing: processingReducer,
-    //production: productionReducer,
-    //storage: storageReducer,
-    //ttb: ttbReducer,
+    processing: processingReducer,
+    production: productionReducer,
+    storage: storageReducer,
+    ttb: ttbReducer,
   }
 })
